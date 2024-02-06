2555d901b12fd09725f44469c67d198d499b639f
Shubham Girdhar <girdharshubham@hotmail.com>
2024-02-06 06:07:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ App(
     environment="development",
     # use cfnoutput internally instead of using the makefile
     system_master=os.getenv("SYSTEM_MASTER"),
+    # TODO | ingress nginx is integrated through the makefile at them moment, but the ideal way would be to do it through aws cdk. aws_eks supports helm charts.
+    # The missing bit is to integrate the cfnoutput to work with aws_eks helm chart bit
     helm_charts={
         "IngressNginx": {
             "chart": "ingress-nginx",
